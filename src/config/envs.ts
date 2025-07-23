1e6f0e54d402717d4b782163fd65e194a65481f2
Emilio Ibarra <emilio.ibarra.270@gmail.com>
2025-07-23 17:13:39 -0600

    feat: add payment route and webhook connection

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -7,6 +7,7 @@ interface EnvVars {
   STRIPE_SUCCESS_URL: string;
   STRIPE_CANCEL_URL: string;
   STRIPE_ENDPOINT_SECRET: string;
+  NATS_SERVERS: string[];
 }
 
 const envsSchema = joi
@@ -19,7 +20,10 @@ const envsSchema = joi
   })
   .unknown(true);
 
-const { error, value } = envsSchema.validate(process.env);
+const { error, value } = envsSchema.validate({
+  ...process.env,
+  NATS_SERVERS: process.env.NATS_SERVERS?.split(','),
+});
 
 if (error) throw new Error(`Config validation error: ${error.message}`);
 
@@ -31,4 +35,5 @@ export const envs = {
   stripeSuccessUrl: envVars.STRIPE_SUCCESS_URL,
   stripeCancelUrl: envVars.STRIPE_CANCEL_URL,
   stripeEndpointSecret: envVars.STRIPE_ENDPOINT_SECRET,
+  natsServers: envVars.NATS_SERVERS,
 };
